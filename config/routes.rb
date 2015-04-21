26dc1c0a5638cb520f0c7561971633980cd6102c
Doug Warren <git@dpw67.oib.com>
2015-04-21 11:04:12 -0500

    Uploaded assets are now served through the AssetsController for extra security

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
     resources :tickets
   end
   
+  resources :assets, only: [:show], path: :files
+  
 end
 
---
+++ spec/factories/asset_factory.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :asset do
+    transient do
+      file "spec/fixtures/speed.txt"
+    end
+
+    asset { File.open file }
+  end
+
+end
